86102feba5ed8d52cc0bb09b0c562d61024d1a70
CrabVS <139277442+CrabVS@users.noreply.github.com>
2023-08-23 15:58:23 +0200

    Add gameboard module

---
+++ src/gameboard/gameboard.js
@@ -0,0 +1,11 @@
+class Gameboard {
+  constructor() {
+    this.gridSize = 10; // Functions as either the height or width
+  }
+
+  // should be able to place ships at specific coordinates by calling ship factory
+  // a receiveAttack func to check if ship was hit or not and to update the tile
+  // report if all ships were hit or not
+}
+
+export default Gameboard;
